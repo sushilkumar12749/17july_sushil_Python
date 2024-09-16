e0ba85ac0791f9038b106a1044d078cea1bc2a63
Tatva Sushil <157198627+sushilkumar12749@users.noreply.github.com>
2024-09-16 00:16:00 -0700

    Delete Assignments directory

--- Assignments/Fundamentals Of Python Language/programme12.py
+++
@@ -1,5 +0,0 @@
-# Write a Python program to calculate the length of a string. 
-
-my_string = "Hello, world!"
-string_length = len(my_string)
-print(f"The length of the string is: {string_length}")
--- Assignments/task/first.py
+++
@@ -1,9 +0,0 @@
-Name=input("Enter Your Name")
-#print("Your name is" Name)
-DOB=input("Enter Your DOB")
-
-city=input("Enter Your city name")
-
-Address=input("Enter Your Address")
-
-

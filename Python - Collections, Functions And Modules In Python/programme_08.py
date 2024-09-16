3686944b73c070b434e7273fc45d32c7c5f430ba
Tatva Sushil <157198627+sushilkumar12749@users.noreply.github.com>
2024-09-16 00:19:12 -0700

    Update programme_08.py

--- Python - Collections, Functions And Modules In Python/programme_08.py
+++ Python - Collections, Functions And Modules In Python/programme_08.py
@@ -1,7 +1,7 @@
 #Write a Python function that takes two lists and returns true if they have
 #at least one common member. 
 
-a = ['darshan', 'meet', 'avni']
+a = ['sushil', 'sunil', 'saurabh']
 
 if len(a) > 0:
     print("List is not empty")

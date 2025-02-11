bc7462e1ea35357dc4f2215052ef597bcb699d13
Tatva Sushil <157198627+sushilkumar12749@users.noreply.github.com>
2025-02-11 12:26:48 +0530

    Delete htmlproject directory

--- htmlproject/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-from myapp import views
-
-urlpatterns = [
-    path('',views.html1),
-    path('jango/',views.jango),
-    path('doctor/',views.doctor),
-]

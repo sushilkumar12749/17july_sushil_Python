56940a2b872cc988097a18eaa9a1e891f76aec5c
Tatva Sushil <157198627+sushilkumar12749@users.noreply.github.com>
2025-02-11 12:22:47 +0530

    Delete cssproject directory

--- cssproject/cssapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CssappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cssapp'
--- cssproject/cssapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-from cssapp import views
-
-urlpatterns = [
-    path('',views.index),
-    path('basic/',views.basic),
-    path('doctor/',views.doctor),
-]
